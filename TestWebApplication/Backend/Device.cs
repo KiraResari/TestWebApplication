e0c6a7cefdb789247d4f29ed8673be0c85c9a6f0
Kira Resari <kira@tri-tail.com>
2021-12-16 15:03:09 +0100

    Added dynamic table for devices

---
+++ TestWebApplication/Backend/Device.cs
@@ -0,0 +1,14 @@
+﻿namespace TestWebApplication.Backend
+{
+    public class Device
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Device(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ TestWebApplication/Backend/TestDevices.cs
@@ -0,0 +1,11 @@
+﻿namespace TestWebApplication.Backend
+{
+    public static class TestDevices
+    {
+        public static readonly List<Device> list = new List<Device>() {
+            new Device("Test Device 1", "Test Description 1"),
+            new Device("Test Device 2", "Test Description 2"),
+            new Device("Test Device 3", "Test Description 3")
+        };
+    }
+}
